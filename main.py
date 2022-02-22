85402e27fba06f65a4b355887b3b1061f580a744
Ilya Senchukov <senchukov02@gmail.com>
2022-02-22 20:35:07 +0200

    feat: equation solver

--- main.py
+++ main.py
@@ -1,4 +1,41 @@
 import sys
+import math
+
+
+class QuadraticEquationSolver:
+
+    def equation_str(self, a, b, c):
+        return f"({a}) x^2 + ({b}) x + ({c}) = 0"
+
+    def _discriminant(self, a, b, c):
+        return b * b - 4 * a * c
+
+    def _get_roots(self, a, b, discriminant):
+        x1 = (-b + math.sqrt(discriminant)) / (2 * a)
+        x2 = (-b - math.sqrt(discriminant)) / (2 * a)
+        return (x1, x2)
+
+    def solve(self, a, b, c):
+        print("Equation is:", self.equation_str(a, b, c))
+
+        discriminant = self._discriminant(a, b, c)
+        (x1, x2) = self._get_roots(a, b, discriminant)
+
+        number_of_roots = 0
+        if discriminant == 0 or x1 == x2:
+            number_of_roots = 1
+        elif discriminant > 0:
+            number_of_roots = 2
+
+        if not number_of_roots:
+            print("There are no roots")
+            return
+        else:
+            print(f"There are {number_of_roots} roots")
+        
+        print(f"x1 = {x1}")    
+        if x1 != x2:
+            print(f"x2 = {x2}")
 
 
 def input_from_file(filename: str):
@@ -28,8 +65,8 @@ def main(args):
     else:
         input = input_from_command_line()
 
-    # solver = QuadraticEquationSolver()
-    # solver.solve(*input)
+    solver = QuadraticEquationSolver()
+    solver.solve(*input)
 
 if __name__ == "__main__":
     main(sys.argv[1:])    
